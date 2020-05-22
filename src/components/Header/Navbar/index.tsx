fc177029e2ac1623e1630ca547d5abf83b5d544c
Leonardo Oliveira <leonfoliveira@gmail.com>
2020-05-22 18:21:25 -0300

    Add responsiveness to navbar

--- src/components/Header/Navbar/index.tsx
+++ src/components/Header/Navbar/index.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaBars } from 'react-icons/fa';
 
 import NavbarItem from './NavbarItem';
 
 import styles from './Navbar.module.scss';
 
-const Navbar: React.FC = () => (
-  <nav className={styles.container}>
-    <NavbarItem href="/" text="Sales" highlight />
-    <NavbarItem href="/section/drinks" text="Drinks" />
-    <NavbarItem href="/section/fruits" text="Fruits and Vegetables" />
-    <NavbarItem href="/section/hygiene" text="Hygiene and Cleaning" />
-    <NavbarItem href="/section/pet" text="Pet" />
-  </nav>
-);
+const Navbar: React.FC = () => {
+  const [opened, setOpened] = useState(false);
+
+  const handleOpen = () => {
+    setOpened(!opened);
+  };
+
+  return (
+    <nav className={styles.container}>
+      <button type="button" onClick={handleOpen}>
+        <div className={styles.hamburguer}>
+          <FaBars />
+        </div>
+      </button>
+      <div className={`${styles.items}`}>
+        <NavbarItem href="/" text="Sales" highlight />
+        <div className={opened ? styles.opened : styles.closed}>
+          <NavbarItem href="/section/drinks" text="Drinks" />
+          <NavbarItem href="/section/fruits" text="Fruits and Vegetables" />
+          <NavbarItem href="/section/hygiene" text="Hygiene and Cleaning" />
+          <NavbarItem href="/section/pet" text="Pet" />
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
